e2f61af1de59f5015d8b848e29ae80cc2b2cf0e9
Bruno Noboru <bntatematsu@gmail.com>
2022-05-14 20:53:14 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,29 @@
+
+import { ReactComponent as GithubIcons } from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/Bruno-Noboru/dsmovie">
+
+                        <div className="dsmovie-contact-container">
+                            <GithubIcons />
+                            <p className="dsmovie-contact-link">/Bruno-Noboru</p>
+                        </div>
+                    </a>
+
+                </div>
+
+            </nav>
+
+        </header>
+
+    );
+
+}
+
+export default Navbar;
