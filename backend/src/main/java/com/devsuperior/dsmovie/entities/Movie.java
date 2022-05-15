1434fcf9fcdb4022bbe1b2f8546f1e8760a55870
Bruno Noboru <bntatematsu@gmail.com>
2022-05-15 15:49:16 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dsmovie.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="tb_movie")
+@Table(name = "tb_movie")
 public class Movie {
 
 	@Id
@@ -19,10 +23,12 @@ public class Movie {
 	private String image;
 	
 	
-	public Movie() {
-		
-	}
+	@OneToMany(mappedBy = "id.movie")
+	private Set<Score> scores = new HashSet<>();
 
+	public Movie() {
+
+	}
 
 	public Movie(Long id, String title, Double score, Integer count, String image) {
 		this.id = id;
@@ -32,56 +38,49 @@ public class Movie {
 		this.image = image;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getTitle() {
 		return title;
 	}
-
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-
 	public Double getScore() {
 		return score;
 	}
-
 
 	public void setScore(Double score) {
 		this.score = score;
 	}
 
-
 	public Integer getCount() {
 		return count;
 	}
-
 
 	public void setCount(Integer count) {
 		this.count = count;
 	}
 
-
 	public String getImage() {
 		return image;
 	}
 
-
 	public void setImage(String image) {
 		this.image = image;
 	}
+
 	
-	
-	
+	public Set<Score> getScores() {
+		return scores;
+	}
+
 }
